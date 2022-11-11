572f56d74f9ba958a46bb30f7af3771a49f55aac
Victor Oliveira <vggaia10@gmail.com>
2022-11-11 10:11:52 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-const express = require('express')
+const express = require("express");
+const routes = require("./routes");
 
-const app = express()
-
-app.get('/', (request, response) => {
-  response.send('aaaa')
-})
+const app = express();
+app.use(routes);
 
 app.listen(3333, () =>
-  console.log('🔥 Server started at http://localhost:3333')
-)
+  console.log("🔥 Server started at http://localhost:3333")
+);
